24e6f3571d4965b61fe068a7bcbd1a4a26246fe3
Nightterror <zombies_ate_my_family@hotmail.co.nz>
2012-05-10 20:19:20 +1200

    More Bot rotations implemented:
    -All 3 Warlock specs have dps rotations
    -Warlock demon is decided on by spec (This could probably be improved)
    -Warlocks use spellstone instead of firestone
    
    -All 3 Warrior spec rotations implemented
    -Currently sudden death and revenge aren't working
    
    -All 4 Druid specs implemented
    -Bear form and rotation will only be used if tank orders given
    -Currently Eclipse procs aren't working
    
    Misc:
    -Paladins will turn RF off if orders aren't tank or are cleared from tank
    
    Signed-off-by: cala <calaftp@free.fr>

--- src/game/playerbot/PlayerbotWarlockAI.h
+++ src/game/playerbot/PlayerbotWarlockAI.h
@@ -146,6 +146,7 @@ public:
     //void BuffPlayer(Player *target);
 
 private:
+	void CheckDemon();
 
     // CURSES
     uint32 CURSE_OF_WEAKNESS,
@@ -198,7 +199,8 @@ private:
            DETECT_INVISIBILITY,
            CREATE_FIRESTONE,
            CREATE_SOULSTONE,
-           CREATE_HEALTHSTONE;
+           CREATE_HEALTHSTONE,
+		   CREATE_SPELLSTONE;
 
     // DEMON SUMMON
     uint32 SUMMON_IMP,
