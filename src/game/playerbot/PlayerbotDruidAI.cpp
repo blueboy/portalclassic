4773cf0f138a20975bbd5cd1d3a89c1c9695bdc6
kennumen <kvandaele@hotmail.com>
2012-07-17 20:41:33 +0200

    [fix] Added check to see if druid has Revive spell before trying to cast it.
    Signed-off-by: Cala Haryon <calaftp@free.fr>

--- src/game/playerbot/PlayerbotDruidAI.cpp
+++ src/game/playerbot/PlayerbotDruidAI.cpp
@@ -605,7 +605,7 @@ void PlayerbotDruidAI::DoNonCombatActions()
             // Resurrect member if needed
             if (!tPlayer->isAlive())
             {
-                if (CastSpell(REVIVE, tPlayer))
+                if (REVIVE > 0 && CastSpell(REVIVE, tPlayer))
                 {
                     std::string msg = "Resurrecting ";
                     msg += tPlayer->GetName();
@@ -629,7 +629,7 @@ void PlayerbotDruidAI::DoNonCombatActions()
             return;
         if (!m_master->isAlive())
         {
-            if (CastSpell(REVIVE, m_master))
+            if (REVIVE > 0 && CastSpell(REVIVE, m_master))
                 m_ai->TellMaster("Resurrecting you, Master.");
             return;
         }
