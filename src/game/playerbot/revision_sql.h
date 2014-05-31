cmangos/mangos-wotlk

Fix to prevent merge conflicts, when sql changes occur &  provide a more relevant db error message, should the versions  not match.

src/game/playerbot/revision_sql.h
@@ -1,0 +1,1 @@
+#define REVISION_DB_PLAYERBOTAI "required_5_playerbotai_auto_follow"

blueboy/portalclassic

Signed-off-by: Cala Haryon <calaftp@free.fr>

src/game/playerbot/revision_sql.h
@@ -1,0 +1,1 @@
+#define REVISION_DB_PLAYERBOTAI "playerbotai_characters_svdata_002"