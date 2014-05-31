cmangos/mangos-wotlk

[tweak] Merge primorder and secorder into combat_order for db as well as code. Add safety check so only a single primary order can be active. Streamline various combat order code. Merge resist types into combat_order.

src/game/playerbot/revision_sql.h
@@ -1,1 +1,1 @@
-#define REVISION_DB_PLAYERBOTAI "required_5_playerbotai_auto_follow"
+#define REVISION_DB_PLAYERBOTAI "required_6_playerbotai_combatorders"

blueboy/portalclassic

Signed-off-by: Cala Haryon <calaftp@free.fr>

src/game/playerbot/revision_sql.h
@@ -1,1 +1,1 @@
-#define REVISION_DB_PLAYERBOTAI "playerbotai_characters_svdata_002"
+#define REVISION_DB_PLAYERBOTAI "playerbotai_characters_svdata_003"